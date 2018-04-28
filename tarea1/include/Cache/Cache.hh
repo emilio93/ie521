dc6ca3f7467f117439642110a1da884445d4a558
Emilio Rojas <emilio93@gmail.com>
2018-04-27 22:01:22 -0600

    add setters and getters on cache

--- tarea1/include/Cache/Cache.hh
+++ tarea1/include/Cache/Cache.hh
@@ -89,6 +89,48 @@ class Cache {
   static Cache *makeCache(unsigned int size, unsigned int associativity,
                           unsigned int blockSize, CacheRP cacheRP,
                           unsigned int missPenalty);
+
+  unsigned int getSize();
+  unsigned int getAssociativity();
+  unsigned int getBlockSize();
+  CacheRP getCacheRP();
+  unsigned int getMissPenalty();
+
+  void setSize(unsigned int size);
+  void setAssociativity(unsigned int associativity);
+  void setBlockSize(unsigned int blockSize);
+  void setCacheRP(CacheRP cacheRP);
+  void setMissPenalty(unsigned int missPenalty);
+
+  unsigned int getSimResults();
+  unsigned int getInstructions();
+  unsigned int getMemAccesses();
+  float getMissRate();
+  float getRdMissRate();
+  float getCpuTime();
+  float getAvgMemAccessTime();
+  unsigned int getDirtyEvictions();
+  unsigned int getLoadMisses();
+  unsigned int getStoreMisses();
+  unsigned int getTotalMisses();
+  unsigned int getLoadHits();
+  unsigned int getStoreHits();
+  unsigned int getTotalHits();
+
+  void setSimResults(unsigned int simResults);
+  void setInstructions(unsigned int instructions);
+  void setMemAccesses(unsigned int memAccesses);
+  void setMissRate(float missRate);
+  void setRdMissRate(float RdMissRate);
+  void setCpuTime(float cpuTime);
+  void setAvgMemAccessTime(float avgMemAccessTime);
+  void setDirtyEvictions(unsigned int dirtyEvictions);
+  void setLoadMisses(unsigned int loadMisses);
+  void setStoreMisses(unsigned int storeMisses);
+  void setTotalMisses(unsigned int totalMisses);
+  void setLoadHits(unsigned int loadHits);
+  void setStoreHits(unsigned int storeHits);
+  void setTotalHits(unsigned int totalHits);
 };
 
 #endif
--- tarea1/src/Cache/Cache.cpp
+++ tarea1/src/Cache/Cache.cpp
@@ -23,18 +23,86 @@ Cache* Cache::makeCache(unsigned int size, unsigned int associativity,
 std::string Cache::toString() {
   std::string result = "";
   result += "Tamaño: ";
-  result += std::to_string(this->size);
+  result += std::to_string(this->getSize());
 
   result += "\nAsociatividad: ";
-  result += std::to_string(this->associativity);
+  result += std::to_string(this->getAssociativity());
 
   result += "\nTamaño Línea: ";
-  result += std::to_string(this->blockSize);
+  result += std::to_string(this->getBlockSize());
 
   result += "\nPolítica de Remplazo: ";
-  result += cacheRPToString(this->cacheRP);
+  result += cacheRPToString(this->getCacheRP());
 
   result += "\nMiss Penalty: ";
-  result += std::to_string(this->missPenalty);
+  result += std::to_string(this->getMissPenalty());
   return result + "\n";
+}
+
+unsigned int Cache::getSize() { return this->size; }
+unsigned int Cache::getAssociativity() { return this->associativity; }
+unsigned int Cache::getBlockSize() { return this->blockSize; }
+CacheRP Cache::getCacheRP() { return this->cacheRP; }
+unsigned int Cache::getMissPenalty() { return this->missPenalty; }
+
+void Cache::setSize(unsigned int size) { this->size = size; }
+void Cache::setAssociativity(unsigned int associativity) {
+  this->associativity = associativity;
+}
+void Cache::setBlockSize(unsigned int blockSize) {
+  this->blockSize = blockSize;
+}
+void Cache::setCacheRP(CacheRP cacheRP) { this->cacheRP = cacheRP; }
+void Cache::setMissPenalty(unsigned int missPenalty) {
+  this->missPenalty = missPenalty;
+}
+
+unsigned int Cache::getSimResults() { return this->simResults; }
+unsigned int Cache::getInstructions() { return this->instructions; }
+unsigned int Cache::getMemAccesses() { return this->memAccesses; }
+float Cache::getMissRate() { return this->missRate; }
+float Cache::getRdMissRate() { return this->RdMissRate; }
+float Cache::getCpuTime() { return this->cpuTime; }
+float Cache::getAvgMemAccessTime() { return this->avgMemAccessTime; }
+unsigned int Cache::getDirtyEvictions() { return this->dirtyEvictions; }
+unsigned int Cache::getLoadMisses() { return this->loadMisses; }
+unsigned int Cache::getStoreMisses() { return this->storeMisses; }
+unsigned int Cache::getTotalMisses() { return this->totalMisses; }
+unsigned int Cache::getLoadHits() { return this->loadHits; }
+unsigned int Cache::getStoreHits() { return this->storeHits; }
+unsigned int Cache::getTotalHits() { return this->totalHits; }
+
+void Cache::setSimResults(unsigned int simResults) {
+  this->simResults = simResults;
+}
+void Cache::setInstructions(unsigned int instructions) {
+  this->instructions = instructions;
+}
+void Cache::setMemAccesses(unsigned int memAccesses) {
+  this->memAccesses = memAccesses;
+}
+void Cache::setMissRate(float missRate) { this->missRate = missRate; }
+void Cache::setRdMissRate(float RdMissRate) { this->RdMissRate = RdMissRate; }
+void Cache::setCpuTime(float cpuTime) { this->cpuTime = cpuTime; }
+void Cache::setAvgMemAccessTime(float avgMemAccessTime) {
+  this->avgMemAccessTime = avgMemAccessTime;
+}
+void Cache::setDirtyEvictions(unsigned int dirtyEvictions) {
+  this->dirtyEvictions = dirtyEvictions;
+}
+void Cache::setLoadMisses(unsigned int loadMisses) {
+  this->loadMisses = loadMisses;
+}
+void Cache::setStoreMisses(unsigned int storeMisses) {
+  this->storeMisses = storeMisses;
+}
+void Cache::setTotalMisses(unsigned int totalMisses) {
+  this->totalMisses = totalMisses;
+}
+void Cache::setLoadHits(unsigned int loadHits) { this->loadHits = loadHits; }
+void Cache::setStoreHits(unsigned int storeHits) {
+  this->storeHits = storeHits;
+}
+void Cache::setTotalHits(unsigned int totalHits) {
+  this->totalHits = totalHits;
 }
