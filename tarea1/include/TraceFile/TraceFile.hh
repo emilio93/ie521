65cb7e7cc40aa71db23f8639dad384f224a8536e
Emilio Rojas <emilio93@gmail.com>
2018-04-28 18:08:18 -0600

    update TraceFile
    should be ready, removed some unused functions and added documentation

--- tarea1/include/TraceFile/TraceFile.hh
+++ tarea1/include/TraceFile/TraceFile.hh
@@ -12,35 +12,112 @@
 
 #include "TraceLine/TraceLine.hh"
 
+/**
+ * @brief a TraceFile enables reading a .trace.gz file for cache simulation
+ *
+ */
 class TraceFile {
  private:
+  /**
+   * @brief file input stream
+   *
+   */
   std::ifstream file;
+
+  /**
+   * @brief trace file path
+   *
+   */
   std::string filepath;
+
+  /**
+   * @brief a reference to a single line of the trace file
+   *
+   */
   std::string* line;
+
+  /**
+   * @brief stream filtering to read the compressed data
+   *
+   */
   boost::iostreams::filtering_istream in;
 
+  /**
+   * @brief the TraceLine being read
+   *
+   */
   TraceLine* curTraceLine;
 
+  /**
+   * @brief set the Line object
+   *
+   * @param line
+   */
   void setLine(std::string* line);
+
+  /**
+   * @brief set the filepath
+   *
+   * @param filepath
+   */
   void setFilepath(std::string filepath);
 
- public:
-  TraceFile(std::string filepath);
-  ~TraceFile();
-
-  TraceLine* getTraceLine();
-
-  bool nextLine();
-  std::string* currLine();
-
+  /**
+   * @brief resets the file reader to the start of the set filepath
+   *
+   */
   void reset();
 
-  void countLines();
-
+  /**
+   * @brief get the file stream
+   *
+   * @return std::ifstream* file stream
+   */
   std::ifstream* getFile();
-  std::string* getFilepath();
-  std::string* getLine();
+
+  /**
+   * @brief Get the stream filtering
+   *
+   * @return boost::iostreams::filtering_istream* stream filtering
+   */
   boost::iostreams::filtering_istream* getIn();
+
+ public:
+
+  /**
+   * @brief Construct a new Trace File object
+   *
+   * @param filepath
+   */
+  TraceFile(std::string filepath);
+
+  /**
+   * @brief Destroy the Trace File object
+   *
+   */
+  ~TraceFile();
+
+  /**
+   * @brief Retrieves next line and indicates if indeed there was a next line
+   *
+   * @return true if there is a next line
+   * @return false if there is not a next line
+   */
+  bool nextLine();
+
+  /**
+   * @brief get the filepath
+   *
+   * @return std::string* filepath
+   */
+  std::string* getFilepath();
+
+  /**
+   * @brief get the current line
+   *
+   * @return std::string* the last read line
+   */
+  std::string* getLine();
 };
 
 #endif
--- tarea1/src/TraceFile/TraceFile.cpp
+++ tarea1/src/TraceFile/TraceFile.cpp
@@ -4,7 +4,11 @@ TraceFile::TraceFile(std::string filepath) {
   this->curTraceLine = NULL;
   this->line = NULL;
   this->filepath = filepath;
-  this->reset();
+  try {
+    this->reset();
+  } catch (boost::iostreams::zlib_error& e) {
+    throw e;
+  }
 }
 
 TraceFile::~TraceFile() {
@@ -19,10 +23,7 @@ void TraceFile::reset() {
     this->in.push(boost::iostreams::gzip_decompressor());
     this->in.push(this->file);
   } catch (boost::iostreams::zlib_error& e) {
-    std::cout << "Error: " << e.what() << std::endl
-              << boost::iostreams::zlib::data_error << std::endl
-              << e.code() << std::endl
-              << e.error() << std::endl;
+    throw e;
   }
 }
 
@@ -36,35 +37,6 @@ bool TraceFile::nextLine() {
   this->line = str;
 
   return true;
-}
-
-TraceLine* TraceFile::getTraceLine() {
-  // std::cout << this->file;
-  if (this->curTraceLine == NULL) {
-    // this->curTraceLine = TraceLine::makeTraceLine();
-  }
-  return NULL;
-}
-
-std::string* TraceFile::currLine() { return this->getLine(); }
-
-void TraceFile::countLines() {
-  try {
-    boost::iostreams::filtering_istream in;
-    in.push(boost::iostreams::gzip_decompressor());
-    in.push(file);
-    std::string a;
-    int i = 0;
-    for (std::string str; std::getline(in, str);) {
-      i++;
-    }
-    std::cout << std::dec << i << std::endl;
-  } catch (boost::iostreams::zlib_error& e) {
-    std::cout << "Error: " << e.what() << std::endl
-              << boost::iostreams::zlib::data_error << std::endl
-              << e.code() << std::endl
-              << e.error() << std::endl;
-  }
 }
 
 std::ifstream* TraceFile::getFile() { return &this->file; }
