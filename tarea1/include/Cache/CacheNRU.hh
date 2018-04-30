67fb0321aa24f0a61dfc9d1dd913acaabbd5b77d
Emilio Rojas <emilio93@gmail.com>
2018-04-30 02:45:14 -0600

    update NRU cache

--- tarea1/include/Cache/CacheNRU.hh
+++ tarea1/include/Cache/CacheNRU.hh
@@ -10,7 +10,31 @@
 #include "Cache/Cache.hh"
 #include "TraceFile/TraceFile.hh"
 
+
+
+struct NruNode {
+  long int tag;
+  bool nruBit;
+  NruNode(long int tag, bool nruBit) {
+    this->tag = tag;
+    this->nruBit = nruBit;
+  };
+};
+
+typedef std::list<NruNode> NruList;
+typedef std::unordered_map<long int, NruList::iterator> NruMap;
+
 class CacheNRU : public Cache {
+  private:
+
+  // Keeps the nru list
+  std::vector<NruList> nruList;
+
+  // maps the nru List for fast access maps the nru list to the cache
+  std::vector<NruMap> nruMap;
+
+  static bool isNru(const NruNode& nruNode);
+
  public:
   //! Runs the tests on the cache with the given parameters and assign results.
   explicit CacheNRU(unsigned int size, unsigned int associativity,
--- tarea1/src/Cache/CacheNRU.cpp
+++ tarea1/src/Cache/CacheNRU.cpp
@@ -4,9 +4,64 @@ CacheNRU::CacheNRU(unsigned int size, unsigned int associativity,
                    unsigned int blockSize, CacheRP cacheRP,
                    unsigned int missPenalty, TraceFile* tfr)
     : Cache(size, associativity, blockSize, cacheRP, missPenalty, tfr) {
+  this->nruList = std::vector<NruList>();
+  this->nruMap = std::vector<NruMap>();
 
+  // create lru lists and maps for each set
+  for (size_t i = 0; i < this->getAssociativity(); i++) {
+    NruList setN = NruList();
+    this->nruList.push_back(setN);
+
+    NruMap mapN = NruMap();
+    this->nruMap.push_back(mapN);
+    for (size_t j = 0; j < this->cacheLines; j++) {
+      this->nruList.at(i).push_back(NruNode(j, 0));
+      this->nruMap.at(i).insert_or_assign(j, this->nruList.at(i).end());
+    }
+  }
 };
 
-void CacheNRU::access(TraceLine* traceLine){
+void CacheNRU::access(TraceLine* traceLine) {
+  if (this->isHit) {
+    // set nru bit to 0
+    this->nruMap.at(this->index).at(this->tag)->nruBit = 0;
+  } else {
+    // search first 1
+    auto it =
+        std::find_if(std::begin(this->nruList.at(this->index)),
+                     std::end(this->nruList.at(this->index)), this->isNru);
+    if (it != this->nruList.at(this->index).end()) {
+      it->nruBit = 0;
+    } else {
+      // count dirty eviction
+      if (this->cache.at(this->index)
+              .at(this->nruList.at(this->index).front().tag)
+              .dirtyBit &&
+          this->cache.at(this->index)
+              .at(this->nruList.at(this->index).front().tag)
+              .valid) {
+        // add to dirty evictions
+        this->setDirtyEvictions(this->getDirtyEvictions() + 1);
+      }
+      // set all nru bits to 1 and push to front
+      for (auto var : this->nruList.at(this->index)) {
+        var.nruBit = 1;
+      }
 
+      // remove elements
+      this->cache.at(this->index)
+          .erase(this->nruList.at(this->index).front().tag);
+      this->nruMap.at(this->index)
+          .erase(this->nruList.at(this->index).front().tag);
+      this->nruList.at(this->index).pop_front();
+
+      // insert element to nru list, map and also to the cache
+      this->nruList.at(this->index).push_front(NruNode(this->tag, 0));
+      this->nruMap.at(this->index)
+          .insert_or_assign(this->tag, this->nruList.at(this->index).begin());
+      this->cache.at(this->index).insert_or_assign(this->tag, CacheInfo(0, 1));
+    }
+  }
 };
+
+bool CacheNRU::isNru(const NruNode& nruNode) { return nruNode.nruBit; }
